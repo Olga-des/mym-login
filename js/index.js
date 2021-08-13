5be65d91d5b527fa6bb54b8e6918f7fce5866f97
Olga-des <opeshkova@live.com>
2021-08-13 18:52:47 +1000

    javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-function fade(){
-    $('.preloader').fadeOut("slow");
-}
+$(document).ready(function(){
+    $("body.fade").fadeIn(1500);
+      
+    $("#about").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#aboutGrid").offset().top
+        }, 1000);
+    });
+
+    $("#work").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#workGrid").offset().top
+        }, 1000);  
+    });
+
+    function fade(){
+        $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 2000);
+
+  });
